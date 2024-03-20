954155c3818f717244e5bd88e0b80a7a2fac3338
Manjula Guneratne <mguneratne3@gmail.com>
2024-03-20 07:35:00 -0400

    wkhtmltopdf link added

--- index.py
+++ index.py
@@ -10,3 +10,10 @@ today_date = datetime.today().strftime("%d %b, %y")
 
 context = {'my_name': my_name, 'item1': item1, 'item2': item2, 'item3': item3, 'today_date': today_date}
 
+template_loader = jinja2.FileSystemLoader('./')
+template_env = jinja2.Environment(loader=template_loader)
+
+template = template_env.get_template("my-basic-template.html")
+output_text = template.render(context)
+
+pdfkit.configuration(wkhtmltopdf="C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf")
