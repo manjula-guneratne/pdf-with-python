aa911ece5f5f41926c431b255e1c5b51e1580bbf
Manjula <mguneratne3@gmail.com>
2024-04-10 16:50:56 -0400

    text colour added

--- index.py
+++ index.py
@@ -2,7 +2,9 @@ from fpdf import FPDF
 
 pdf = FPDF('P','mm', 'Letter')
 pdf.add_page()
-pdf.set_font('helvetica', size=16)
+
+pdf.set_font('helvetica','BIU', size=16)
+pdf.set_text_color(220,50,50)
 
 pdf.cell(40,10,"hello world", border=True)
 pdf.cell(80,10, "Good bye World!")
