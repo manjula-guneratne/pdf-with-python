ef9a5bb51bc654ec2dc250c46dbeb568375f3230
Manjula Guneratne <mguneratne3@gmail.com>
2024-04-10 07:34:47 -0400

    good bye text added

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
 from fpdf import FPDF
 
-pdf = FPDF()
+pdf = FPDF('P','mm', 'Letter')
 pdf.add_page()
-pdf.set_font('helvetica', size=12)
-pdf.cell(text="hello world")
+pdf.set_font('helvetica', size=16)
+
+pdf.cell(40,10,"hello world", border=True)
+pdf.cell(80,10, "Good bye World!")
+
 pdf.output("hello_world.pdf")
